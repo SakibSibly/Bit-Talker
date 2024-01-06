71bfeafe3d59d740379f0a718ac20c6541ba280c
Nazmus Sakib Sibly <101848801+SakibSibly@users.noreply.github.com>
2024-01-06 18:47:45 +0600

    Create creation.sql

---
+++ database_creation/creation.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE db_bit_talker;
+USE db_bit_talker;
+
+CREATE TABLE IF NOT EXISTS user_info (
+	  id INT PRIMARY KEY auto_increment,
+    name VARCHAR(100),
+    username VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(300)
+);
+
+CREATE TABLE user_chat (
+	  sender_id INT,
+    receiver_id INT,
+    message VARCHAR(500),
+    message_date DATE,
+    message_time TIME,
+    is_taken BOOLEAN,
+    FOREIGN KEY (sender_id) REFERENCES user_info(id),
+    FOREIGN KEY (receiver_id) REFERENCES user_info(id)
+);
